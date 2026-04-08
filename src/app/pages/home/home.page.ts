3c2de48d430842a54e29bc55b40bd0a51d6a0937
zeyad <112281551+zalmajali@users.noreply.github.com>
2026-04-08 10:29:42 +0300

    addAlert

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -182,6 +182,7 @@ async ngOnInit() {
   await this.loadFromApi();
   await this.startPolling();
    await this.checkLoginDataUser();
+   alert("asdasdasd");
   let showLoading = await this.storage.get('showLoading');
   if (showLoading == 0) {
     await this.storage.set('showLoading', '1');
